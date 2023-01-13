d6d0897691bb36f287836c26971a9399d13e932c
Honesty <honestymoore77@gmail.com>
2023-01-12 18:52:44 -0600

    revised

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
    
-        //the card options
-        const cardArray = [
+        
+    const cardArray = [
             {
                 name: 'naruto',
                 img: 'images/naruto.jpg'
@@ -51,17 +51,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 img: 'images/kidsasuke.jpg'
             }
         ]
-    //randomization
+    
     cardArray.sort(() => 0.5 - Math.random())
 
-    //selectors
+    
     const grid = document.querySelector('.grid')
     const resultDisplay = document.querySelector('#result')
     let cardsPicked = []
     let cardsPickedId = []
     let cardsWon = []
     
-    //gameboard with flipCard function
+    
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             const card = document.createElement('img')
@@ -72,35 +72,34 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     
-    //check matches
+    
     function checkForMatch() {
         const cards = document.querySelectorAll('img')
         const optionOneId = cardsPickedId[0]
         const optionTwoId = cardsPickedId[1]
-        //make a match
+        
         if (cardsPicked[0] === cardsPicked[1]) {
-            window.alert('Nice job! Keep going and you will be the next Hokage!')
+            
             cards[optionOneId].setAttribute('src', 'images/white.png')
             cards[optionTwoId].setAttribute('src', 'images/white.png')
             cards[optionOneId].removeEventListener('click', flipCard)
             cards[optionTwoId].removeEventListener('click', flipCard)
             cardsWon.push(cardsPicked)
-        //no match made
+        
         } else {
             cards[optionOneId].setAttribute('src', 'images/frontcard.png')
             cards[optionTwoId].setAttribute('src', 'images/frontcard.png')
-            window.alert('No match! Follow your ninja way!')
         }
-        //all matches made
+        
         cardsPicked = []
         cardsPickedId = []
         resultDisplay.textContent = cardsWon.length
         if (cardsWon.length === cardArray.length/2) {
         resultDisplay.textContent = 'Well done, young shinobi!'
-        }
+        } 
     } 
    
-    //flipping cards
+   
     function flipCard() {
         const cardId = this.getAttribute('data-id')
         cardsPicked.push(cardArray[cardId].name)
@@ -110,6 +109,9 @@ document.addEventListener('DOMContentLoaded', () => {
             setTimeout(checkForMatch, 300)
         }
     }
-    //initialize
+   
     createBoard()
+    
+    document.getElementById("#alert").addEventListener("click", message)
+
 })
